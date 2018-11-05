bdb7e8ed85a4709bfafe7522ce397b965454a3b4
Stephen Fink <sjfink@us.ibm.com>
2018-11-05 17:48:33 -0500

    stuff

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM qa_base
 
 ADD . /root/DrQA
 
+RUN mkdir /root/DrQA/data
+
 RUN cd /root/DrQA &&  \
     pip install -r requirements.txt  \
     &&  python setup.py develop
--- run-debug.sh
+++ run-debug.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --privileged -v /tmp:/tmp -p 5000:5000 -it drqa /bin/bash
+docker run --privileged -v /Users/sjfink/git/DrQA-data:/root/DrQA/data -p 5000:5000 -it drqa /bin/bash
